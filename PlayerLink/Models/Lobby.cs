33c5bcc716abee250c804c180919ae4135b1814b
HAPPYSACKS <74272531+HAPPYSACKS@users.noreply.github.com>
2023-04-15 16:10:35 -0600

    Made the Lobby include Users and the sample data

--- PlayerLink/Models/Lobby.cs
+++ PlayerLink/Models/Lobby.cs
@@ -2,12 +2,35 @@ namespace PlayerLink.Models {
     public class Lobby
     {
         public int LobbyID { get; set; }
-        public int NumPlayers {get; set; }
-        public int MissingPlayers {get; set; }
+        public int NumPlayers { get; set; } = 0;
+        public int MissingPlayers { get; set; }
         public string Game { get; set; } = "";
-        public string LobbyName { get; set; } ="";
+        public string LobbyName { get; set; } = "";
         public List<string> DesiredTags { get; set; } = new List<string>();
         public List<string> UndesiredTags { get; set; } = new List<string>();
         public string DescribeYourself { get; set; } = "";
+        public List<User> Users { get; set; } = new List<User>();
+        public int MaxUsers { get; set; } = 5; // Set the maximum number of users allowed in a lobby
+
+        public bool IsFull()
+        {
+            return Users.Count >= MaxUsers;
+        }
+
+        public bool AddUser(User user)
+        {
+            if (!IsFull())
+            {
+                Users.Add(user);
+                UpdateMissingPlayers();
+                return true;
+            }
+            return false;
+        }
+
+        public void UpdateMissingPlayers()
+        {
+            MissingPlayers = MaxUsers - Users.Count;
+        }
     }
 }
--- PlayerLink/Services/LobbyDataService.cs
+++ PlayerLink/Services/LobbyDataService.cs
@@ -6,9 +6,11 @@ namespace PlayerLink.Services
     public class LobbyDataService
     {
         public List<Lobby> Lobbies { get; set; }
+        private UserDataService UserDataService { get; set; }
 
         public LobbyDataService()
         {
+            UserDataService = new UserDataService();
             Lobbies = CreateSampleLobbies();
         }
         public event Action OnLobbyAdded;
@@ -16,46 +18,53 @@ namespace PlayerLink.Services
         public void AddLobby(Lobby lobby)
         {
             Lobbies.Add(lobby);
+            lobby.UpdateMissingPlayers();
             OnLobbyAdded?.Invoke();
         }
 
-
         private List<Lobby> CreateSampleLobbies()
         {
+            var userAlice = UserDataService.GetUserByUsername("Alice");
+            var userBob = UserDataService.GetUserByUsername("Bob");
+            var userCharlie = UserDataService.GetUserByUsername("Charlie");
+
             return new List<Lobby>
             {
                 new Lobby
                 {
                     LobbyID = 1,
                     NumPlayers = 2,
-                    MissingPlayers = 3,
+                    MaxUsers = 5,
                     Game = "Valorant",
                     LobbyName = "Lobby Alpha",
                     DesiredTags = new List<string> { "Casual", "Friendly" },
                     UndesiredTags = new List<string> { "Competitive", "Toxic" },
-                    DescribeYourself = "A relaxed and friendly environment for casual players."
+                    DescribeYourself = "A relaxed and friendly environment for casual players.",
+                    Users = new List<User> { userAlice, userBob }
                 },
                 new Lobby
                 {
                     LobbyID = 2,
                     NumPlayers = 1,
-                    MissingPlayers = 4,
+                    MaxUsers = 5,
                     Game = "League of Legends",
                     LobbyName = "Lobby Beta",
                     DesiredTags = new List<string> { "Competitive", "Teamwork" },
                     UndesiredTags = new List<string> { "Trolling", "AFK" },
-                    DescribeYourself = "Focused on competitive gameplay and team coordination."
+                    DescribeYourself = "Focused on competitive gameplay and team coordination.",
+                    Users = new List<User> { userAlice }
                 },
                 new Lobby
                 {
                     LobbyID = 3,
                     NumPlayers = 1,
-                    MissingPlayers = 1,
+                    MaxUsers = 2,
                     Game = "League of Legends",
                     LobbyName = "Lobby Gamma",
                     DesiredTags = new List<string> { "Strategy", "Competitive" },
                     UndesiredTags = new List<string> { "Solo", "Ragequit" },
-                    DescribeYourself = "I need a duo who is female."
+                    DescribeYourself = "I need a duo who is female.",
+                    Users = new List<User> { userCharlie }
                 }
             };
         }
